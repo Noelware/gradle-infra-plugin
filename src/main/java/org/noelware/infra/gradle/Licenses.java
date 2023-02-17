b8f80f48902c62cd34b6c67b263a5b923bdac2e2
Noel <cutie@floofy.dev>
2023-02-17 13:00:27 -0700

    fix license

--- src/main/java/org/noelware/infra/gradle/Licenses.java
+++ src/main/java/org/noelware/infra/gradle/Licenses.java
@@ -1,5 +1,5 @@
 /*
- * gradle-infra-plugin: 🐻‍❄️🐘 Gradle plugin to configure sane defaults for Noelware's Gradle projects
+ * gradle-infra-plugin: Gradle plugin to configure sane defaults for Noelware's Gradle projects
  * Copyright (c) 2023 Noelware, LLC. <team@noelware.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
